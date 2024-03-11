dbedde71c874472784f95547b2334f82dcaee0fe
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-03-11 16:19:23 +0100

    commit

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -124,24 +124,21 @@ console.log(itemId)
     });
 };
 
-// const updateOnePlace = (req, res) => {
-//   const itemId = req.body.id; // Assuming the ID is in the request body
-//   const updatedData = req.body.updatedData; // Assuming updatedData holds the fields to be updated
-  
-//   PlaceModel.findByIdAndUpdate(itemId, updatedData, { new: true })
-//     .then((updatedItem) => {
-//       if (updatedItem) {
-//         res.status(200).json({ message: 'Item updated successfully', updatedItem });
-//       } else {
-//         res.status(404).json({ error: 'Item not found' });
-//       }
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ error: 'Internal server error' });
-//     });
-// };
 
 
+// PlaceModel.findByIdAndUpdate(id, req.body, { new: true }): This method is provided by Mongoose and is used to find a document by its ID (id) and update it with the data provided in req.body. The { new: true } option ensures that the updated document is returned after the update operation.
+
+const editPlace = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const updatedUser = await PlaceModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json({user: updatedUser, message: 'Item updated successfully' });
+    // res.json({ user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 
 const userLogin = async (req, res) => {
@@ -372,4 +369,4 @@ const uploadLink  = async (req, res)=>{
 }
 
 
-module.exports = { registerUser, userLogin, getDashboard, password, resetPassword, place, uploadFiles, uploadLink, getAllUser, getUserPlace, deleteOne, deletePlace }
+module.exports = { registerUser, userLogin, getDashboard, password, resetPassword, place, uploadFiles, uploadLink, getAllUser, getUserPlace, deleteOne, deletePlace, editPlace }
--- route/user.route.js
+++ route/user.route.js
@@ -1,32 +1,23 @@
-const express = require ('express')
-const router = express.Router()
+// user.routes.js
 
-const {registerUser, userLogin,  getDashboard, password, resetPassword, place, uploadFiles, uploadLink, getAllUser, getUserPlace, deleteOne, deletePlace} = require('../controller/user.controller')
+const express = require('express');
+const router = express.Router();
+const { registerUser, userLogin, getDashboard, password, resetPassword, place, uploadFiles, uploadLink, getAllUser, getUserPlace, deleteOne, deletePlace } = require('../controller/user.controller');
+const { editPlace } = require('../controller/user.controller');
 
+router.post("/signup", registerUser);
+router.post("/login", userLogin);
+router.post('/password', password);
+router.post('/reset', resetPassword);
+router.post('/place', place);
+router.post('/upload', uploadFiles);
+router.post('/link', uploadLink);
+router.delete('/deleteModel', deleteOne);
+router.delete('/deleteUserPlace', deletePlace);
+router.put('/editPlace/:id', editPlace); // Define the route with ID parameter
 
-router.post("/signup", registerUser)
-router.post("/login", userLogin)
-router.post('/password', password)
-router.post('/reset', resetPassword)
-router.post('/place', place)
-router.post('/upload', uploadFiles)
-router.post('/link', uploadLink)
-router.delete('/deleteModel', deleteOne)
-router.delete('/deleteUserPlace', deletePlace)
+router.get("/dashboard", getDashboard);
+router.get('/getUser', getAllUser);
+router.get('/getPlace', getUserPlace);
 
-
-
-// router.put('/updateModel', updateOne)
-// router.put('/update', updateOnePlace)
-
-
-
-router.get("/dashboard", getDashboard)
-router.get('/getUser', getAllUser)
-router.get('/getPlace', getUserPlace)
-
-
-
-
-
-module.exports = router
+module.exports = router;
